348490e95e3c187662c5305d0512acec292136f2
pablocarrara <pablocarrara@yahoo.com.ar>
2022-12-10 23:51:49 -0300

    First template and view

--- movies/urls.py
+++ movies/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
+
+urlpatterns = [path('',views.home),
+                path('movies/', views.movies),
+                path('admin/', admin.site.urls)
+
 ]
